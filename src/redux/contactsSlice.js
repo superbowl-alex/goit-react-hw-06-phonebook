cfd6d9db509dbb52784982dbbaede2695cbf6761
Oleksandr <superbowl@ukr.net>
2022-11-01 22:16:45 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,19 +1,6 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
-import Notiflix from 'notiflix';
-Notiflix.Notify.init({
-  width: '500px',
-  position: 'center-top',
-  closeButton: true,
-  fontFamily: 'Comic Sans MS',
-  fontSize: '24px',
-  warning: {
-    background: 'rgb(255, 240, 245)',
-    textColor: 'rgb(40, 70, 219)',
-    notiflixIconColor: 'rgb(205, 92, 92)',
-  },
-});
 
 const initialContactsState = {
   items: [],
